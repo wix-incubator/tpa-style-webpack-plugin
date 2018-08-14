f8ad20da0edbd314be28270c1f3280e936a23523
JosephDSCS <josephdsc@wix.com>
2018-08-14 17:40:32 +0300

    Corrected customSyntaxHelper File Name

--- src/runtime/customSyntaxHelper2.ts
+++ src/runtime/customSyntaxHelper2.ts
@@ -5,11 +5,9 @@ export class CustomSyntaxHelper {
   public setVars(vars: { [key: string]: string }) {
     this.vars = {...vars};
   }
-
   public setCustomSyntax(customSyntax: string[]) {
     this.customSyntaxStrs = [...customSyntax];
   }
-
   public getValue(varName: string) {
     return this.vars[varName];
   }
