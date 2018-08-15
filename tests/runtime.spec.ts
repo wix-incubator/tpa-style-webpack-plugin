96fe497d65ca48131cc2dbfbeaa10d9c7a9d05d0
Felix Mosheev <felixm@wix.com>
2018-08-15 22:30:09 +0300

    Add fallback tests

--- tests/runtime.spec.ts
+++ tests/runtime.spec.ts
@@ -391,7 +391,7 @@ describe('runtime', () => {
     expect(css).toContain(expectedCss);
   });
 
-  it('should return first none falsy value', () => {
+  it('fallback should return first none falsy value', () => {
     const newStyleParams = clonedWith(styleParams, {
       numbers: {},
       colors: {},
@@ -401,189 +401,15 @@ describe('runtime', () => {
     const expectedCss = `.first-none-falsy {--first_none_falsy: rgb(255, 0, 0); color: rgb(255, 0, 0)}`;
     expect(css).toContain(expectedCss);
   });
+
+  it('fallback should support multiple values', () => {
+    const newStyleParams = clonedWith(styleParams, {
+      numbers: {},
+      colors: {},
+      fonts: {}
+    });
+    const css = getProcessedCss({styleParams: newStyleParams, siteColors, siteTextPresets}, {});
+    const expectedCss = `.fallback-multiple-vals {color: ${getSiteColor('color-1', siteColors)};}`;
+    expect(css).toContain(expectedCss);
+  });
 });
-
-//   it('should return first none falsy value', () => {
-//       const css = '.foo {--my_var3: red; color: "fallback(color(--my_var3), color(color-1))";}';
-//       driver
-//           .given.css(css)
-//           .given.styleParams({
-//           numbers: {},
-//           colors: {},
-//           fonts: {}
-//       })
-//           .given.siteTextPresets({});
-
-//       return driver.when.init()
-//           .then(() => {
-//               expect(driver.get.overrideStyleCallArg()).to.equal('.foo{--my_var3: red;color: rgb(255, 0, 0);}');
-//           });
-//   });
-
-//   it('should support multiple values', () => {
-//       const css = '.foo {color: "fallback(--my_var2, --my_var3, color(color-1))";}';
-//       driver
-//           .given.css(css)
-//           .given.styleParams({
-//           numbers: {},
-//           colors: {},
-//           fonts: {}
-//       })
-//           .given.siteTextPresets({});
-
-//       return driver.when.init()
-//           .then(() => {
-//               expect(driver.get.overrideStyleCallArg()).to.equal('.foo{color: #FFFFFF;}');
-//           });
-//   });
-// });
-
-// describe('As Standalone', () => {
-//   beforeEach(() => {
-//       const css = `.foo {bar: 4; color: "color(color-1)"}`;
-
-//       driver
-//           .given.css(css)
-//           .given.styleParams(null)
-//           .given.siteTextPresets(null)
-//           .given.resetSiteColors()
-//           .given.declarationReplacerPlugin((key, val) => ({
-//           key,
-//           value: '#' + val + '#'
-//       }));
-//   });
-
-//   describe('withoutStyleCapabilites', () => {
-//       it('should not apply css functions', () => {
-//           driver.given.withoutWixStyles();
-//           return driver.when.init().then(() => {
-//               expect(driver.get.overrideStyleCallArg()).to.equal('.foo{bar: #4#;color: #"color(color-1)"#;}');
-//           });
-//       });
-//   });
-
-//   describe('inStandaloneMode', () => {
-//       beforeEach(() => {
-//           driver.given.inStandaloneMode();
-//       });
-
-//       it('should finish init', () => {
-//           return driver.when.init().then(() => {
-//               expect(driver.get.overrideStyleCallArg()).to.equal('.foo{bar: #4#;color: #"color(color-1)"#;}');
-//           });
-//       });
-
-//       it('should not apply css functions', () => {
-//           return driver.when.init().then(() => {
-//               expect(driver.get.overrideStyleCallArg()).to.equal('.foo{bar: #4#;color: #"color(color-1)"#;}');
-//           });
-//       });
-//   });
-// });
-
-// describe('In Editor', () => {
-//   beforeEach(() => {
-//       driver.given.inEditorMode();
-//   });
-
-//   it('should update style on style change event', () => {
-//       const css = `.foo { --bar: "color(color-4)"; color: "color(--bar)"}`;
-
-//       driver.given.css(css);
-
-//       return driver.when.init()
-//           .then(() => {
-//               driver.given.styleParams({
-//                   colors: {
-//                       bar: {value: '#ffffff'}
-//                   }
-//               });
-//           })
-//           .then(driver.when.updateStyleParams)
-//           .then(() => {
-//               expect(driver.get.overrideStyleCallArg(1)).to.equal('.foo{--bar: #717070;color: #ffffff;}');
-//           });
-//   });
-
-//   describe('Enhanced mode', () => {
-//       const color = '"join(darken(color(color-9), 0.5), 0.5, color(color-10), 0.5)"';
-//       const borderWidth = '"unit(number(--borderWidth), string(px))"';
-//       const borderColor = '"withoutOpacity(opacity(color(color-1), 0.5))"';
-//       const font = `"font({theme: 'Body-M', size: '30px'})"`;
-//       const fontVar = `--fontVar`;
-//       const fontWithUnderline = `"font(${fontVar})"`;
-//       const underline = `"underline(${fontVar})"`;
-
-//       beforeEach(() => {
-//           driver.given.cssVarsSupported(true)
-//               .given
-//               .css(`.foo {color: ${color}; border: ${borderWidth} solid ${borderColor}; font: ${font}; font: ${fontWithUnderline}`)
-//               .given.styleParams({
-//               numbers: {borderWidth: 42},
-//               fonts: {
-//                   fontVar: {
-//                       value: `font-family:'mr de haviland','cursive';`,
-//                       index: 93,
-//                       cssFontFamily: `'mr de haviland','cursive'`,
-//                       family: 'mr de haviland',
-//                       fontParam: true,
-//                       size: 0,
-//                       style: {
-//                           bold: false,
-//                           italic: false,
-//                           underline: true
-//                       }
-//                   }
-//               }
-//           })
-//               .given.siteTextPresets({
-//               'Body-M': {
-//                   editorKey: 'font_8',
-//                   fontFamily: 'raleway',
-//                   lineHeight: '1.4em',
-//                   size: '17px',
-//                   style: 'normal',
-//                   value: 'font:normal normal normal 17px/1.4em raleway,sans-serif;',
-//                   weight: 'normal'
-//               }
-//           });
-//       });
-
-//       it('should change custom syntax to native vars', () => {
-//           return driver.when.init()
-//               .then(() => expect(driver.get.overrideStyleCallArg()).to
-//                   .equal(`.foo{color: var(--${hash(color)});border: var(--${hash(borderWidth)}) solid var(--${hash(borderColor)});font: var(--${hash(font)});font: var(--${hash(fontWithUnderline)});text-decoration: var(--${hash(underline)});}`));
-//       });
-
-//       it('should evaluate custom functions on style update', () => {
-//           const newValues = {
-//               number: 42,
-//               color: '#000000'
-//           };
-//           return driver.when.init()
-//               .then(() => {
-//                   driver.given.siteColor('color-1', newValues.color)
-//                       .given.siteColor('color-9', '#0000FF');
-//               })
-//               .then(driver.when.updateStyleParams)
-//               .then(() => {
-//                   expect(driver.get.updateCssVarsCallArg(1)).to
-//                       .eql({
-//                           [`--${hash(color)}`]: 'rgb(0, 255, 128)',
-//                           [`--${hash(borderWidth)}`]: `${newValues.number}px`,
-//                           [`--${hash(borderColor)}`]: 'rgb(0, 0, 0)',
-//                           [`--${hash(font)}`]: 'normal normal normal 30px/1.4em raleway,sans-serif',
-//                           [`--${hash(fontWithUnderline)}`]: 'normal normal normal 17px/1.4em mr de haviland,cursive',
-//                           [`--${hash(underline)}`]: 'underline'
-//                       });
-//               });
-//       });
-
-//       it('should allow to override shouldUseCssVars by options', () => {
-//           return driver
-//               .when.init({shouldUseCssVars: false})
-//               .then(() => expect(driver.get.overrideStyleCallArg()).to
-//                   .equal(`.foo{color: rgb(128, 255, 0);border: 42px solid rgb(255, 255, 255);font: normal normal normal 30px/1.4em raleway,sans-serif;font: normal normal normal 17px/1.4em mr de haviland,cursive;text-decoration: underline;}`));
-//       });
-//   });
-// });
