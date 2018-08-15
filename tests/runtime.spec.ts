46f7a8006c8a8410dcb64a0ae685cbe67afe8418
JosephDSCS <josephdsc@wix.com>
2018-08-15 15:25:22 +0300

    Adds additional tests

--- tests/runtime.spec.ts
+++ tests/runtime.spec.ts
@@ -127,4 +127,67 @@ describe.only('runtime', () => {
     expect(css).toContain(expectedCss);
     expect(css).toContain(expectedCss2);
   });
+
+  it('should not fail on present with none normal font-variant', () => {
+    const newSiteTextPresets = clonedWith(siteTextPresets,
+      {
+        'Body-M': {
+          editorKey: 'font_8',
+          fontFamily: 'din-next-w01-light',
+          lineHeight: '1.4em',
+          size: '16px',
+          style: 'normal',
+          value: 'font:normal small-caps normal 12px/1.2em play,sans-serif;',
+          weight: 'normal'
+        }
+      });
+    const css = getProcessedCss({ styleParams, siteColors, siteTextPresets: newSiteTextPresets }, {});
+    const expectedCss = '.font-test {font: normal normal normal 12px/1.2em play,sans-serif;}';
+    expect(css).toContain(expectedCss);
+  });
+
+  it('should support double font reference', () => {
+    const newSiteTextPresets = clonedWith(siteTextPresets,
+      {
+        'Body-M': {
+          displayName: 'Paragraph 2',
+          editorKey: 'font_8',
+          fontFamily: 'din-next-w01-light',
+          lineHeight: '1.4em',
+          size: '16px',
+          style: 'normal',
+          value: 'font:normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif',
+          weight: 'normal'
+        }
+      });
+    const css = getProcessedCss({ styleParams, siteColors, siteTextPresets: newSiteTextPresets }, {});
+    const expectedCss = '.font-test2 {--some-font: normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif; font: normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;text-decoration: ;}';
+    expect(css).toContain(expectedCss);
+  });
+
+  // failing ::after is not included in the CSS
+  xit('should not calculate empty strings', () => {
+    const css = getProcessedCss({ styleParams, siteColors, siteTextPresets }, {});
+    const expectedCss = `.font-test3::after {content: "t";}`;
+    expect(css).toContain(expectedCss);
+  });
+
+  it('should calculate nested functions', () => {
+    const css = getProcessedCss({ styleParams, siteColors, siteTextPresets }, {});
+    const expectedCss = '.font-test4 {--var: #F3F3F3; color:rgba(255, 255, 255, 0.5);}';
+    expect(css).toContain(expectedCss);
+  });
+
+  it('opacity with default value', () => {
+    const css = getProcessedCss({ styleParams, siteColors, siteTextPresets }, {});
+    const expectedCss = '.foo7 {rule1: rgba(255, 0, 0, 0.5); --lala: #FF0000;}';
+    expect(css).toContain(expectedCss);
+  });
+
+  // failing rule an d rule3 are not in the CSS
+  xit('color transformation', () => {
+    const css = getProcessedCss({ styleParams, siteColors, siteTextPresets }, {});
+    const expectedCss = '.foo8 {rule: bar; rule3: baz; rule4: #FFFFFF; rule5: #F3F3F3;}';
+    expect(css).toContain(expectedCss);
+  });
 });
