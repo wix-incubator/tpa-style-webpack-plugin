bb02df0333beb77e041cff1f54002d279453ebe4
Eran Shabi <eran514@gmail.com>
2018-12-20 16:14:53 +0200

    make runtime.js support import from typescript

--- runtime.js
+++ runtime.js
@@ -1,1 +1,11 @@
-export * from './dist/runtime/fakeMain';
+// The following code is compiled `export * from './dist/runtime/fakeMain';` in order to support typescript
+
+const fakeMain = require('./dist/runtime/fakeMain');
+Object.keys(fakeMain).forEach(key => {
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return fakeMain[key];
+    }
+  });
+});
