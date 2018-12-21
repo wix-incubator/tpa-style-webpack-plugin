beb56ffcf859722b555fb32f0bf864e069e5ca47
Felix Mosheev <felixm@wix.com>
2018-12-21 14:53:12 +0200

    Update comment

--- runtime.js
+++ runtime.js
@@ -1,4 +1,4 @@
-// The following code is compiled `export * from './dist/runtime/fakeMain';` in order to support typescript
+// The following code is compiled `export * from './dist/runtime/fakeMain';` in order to support commonjs
 
 const fakeMain = require('./dist/runtime/fakeMain');
 Object.keys(fakeMain).forEach(key => {
