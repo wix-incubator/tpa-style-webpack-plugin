32e1177c50ef0ecb92c2493c9029f321d7d2d032
Felix Mosheev <felixm@wix.com>
2019-01-15 23:40:40 +0200

    Add bundle size test

---
+++ tests/runtime-size.spec.ts
@@ -0,0 +1,29 @@
+import * as path from 'path';
+import {clearDir} from './helpers/clear-dir';
+import {runWebpack} from './helpers/run-webpack';
+
+describe('runtime size', () => {
+  const outputDirPath = path.resolve(__dirname, './output/runtime-size');
+  const entryName = 'app';
+
+  beforeAll(async () => {
+    await clearDir(outputDirPath);
+  });
+
+  it('should throw when lib size exceeds 25kb', () => {
+    return runWebpack({
+      output: {
+        path: path.resolve(outputDirPath),
+        libraryTarget: 'commonjs'
+      },
+      performance: {
+        maxEntrypointSize: 25 * 1024,
+        hints: 'error'
+      },
+      mode: 'production',
+      entry: {
+        [entryName]: './runtime.js'
+      }
+    });
+  });
+});
