9fe2db151ea1a4fbbde05c3f78c7cbfcc2d2e572
ronnyr <ronnyr@wix.com>
2019-08-28 15:33:37 +0300

    export fake getStaticCss

--- src/runtime/fakeMain.ts
+++ src/runtime/fakeMain.ts
@@ -4,3 +4,7 @@ import {IOptions} from './main';
 exports.getProcessedCss = function getProcessedCss(styles: IStyles, options: IOptions): string {
   return '';
 };
+
+exports.getStaticCss = function getStaticCss(): string {
+  return '';
+};
