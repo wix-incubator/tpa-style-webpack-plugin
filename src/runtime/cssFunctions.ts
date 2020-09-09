fcc5fc9cbb1ed583bdd30de922665f0672034095
liorMar <lior.marzouk@gmail.com>
2020-09-09 15:56:08 +0300

    a fix to a bug in wix ui tpa and stylable mixing

--- src/runtime/cssFunctions.ts
+++ src/runtime/cssFunctions.ts
@@ -58,6 +58,8 @@ export const cssFunctions = {
       };
     } else if (tpaParams.fonts[font]) {
       fontValue = tpaParams.fonts[font];
+    } else if (typeof font === 'string' && font.indexOf('font:') === 0) {
+      return font.slice(5, font.length - 1);
     } else {
       return escapeHtml(font);
     }
